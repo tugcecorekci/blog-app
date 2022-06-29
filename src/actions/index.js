07e6370c69b46bdab4069cf5c6ee83d5f3719646
tugcecorekci <tugcecorekcioglu@gmail.com>
2022-06-29 13:07:43 +0200

    First release of Blog app with React

---
+++ src/actions/index.js
@@ -0,0 +1,68 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+import _ from "lodash"
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts())
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
+}
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts')
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+}
+
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    dispatch({ type: 'FETCH_USER', payload: response.data })
+}
+
+
+/*
+memoize ile çözüm
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+const response = await jsonPlaceholder.get(`/users/${id}`)
+dispatch({ type: 'FETCH_USER', payload: response.data })
+})
+
+*/
+
+/*
+long version without memoize
+
+export const fetchPosts = async () => {
+     return async (dispatch) => {
+         const response = await jsonPlaceholder.get('/posts')
+
+         dispatch({ type: 'FETCH_POST', payload: response })
+     }
+ }
+
+*/
+
+/*
+bad approach!
+
+const promise = await jsonPlaceholder.get('/posts')
+
+    return {
+        type: 'FETCH_POSTS',
+        payload: promise
+    }
+
+*/
+
+/* 
+chain alternatifi
+
+const userIds = _.uniq(_.map(getState().posts, 'userId'))
+userIds.forEach(id => dispatch(fetchUser(id)))
+
+*/
